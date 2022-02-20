0404b5eea28fa0210e6435dc23984343fa4e39b8
Daniel Ochoa <danielochoa@Daniels-MacBook-Pro.local>
2022-02-19 22:09:45 -0800

    Update

--- CougsHelpCougs/CougsHelpCougs/User.cpp
+++ CougsHelpCougs/CougsHelpCougs/User.cpp
@@ -16,3 +16,34 @@ User compare_users_hobby(User cougUsers[], int x)
 		
 
 }
+
+User compare_users_academic_interest(User cougUsers[], int x)
+{
+	int n = 0;
+	int len = sizeof(cougUsers);
+	while(n < len -1)
+	{
+		
+        if (cougUsers[x].getacademic_interest() == cougUsers[n].getacademic_interest())
+		{
+			return cougUsers[n+1];
+		}
+	}
+		
+
+}
+
+User compare_users_recreation(User cougUsers[], int x)
+{
+	int n = 0;
+	int len = sizeof(cougUsers);
+	while(n < len -1)
+	{
+		
+        if (cougUsers[x].getrecreation() == cougUsers[n].getrecreation())
+		{
+			return cougUsers[n+1];
+		}
+	}
+		
+
