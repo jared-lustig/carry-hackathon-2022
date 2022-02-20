cab209002c58355026c1881d830c643a8e24c839
Daniel Ochoa <danielochoa@Daniels-MacBook-Pro.local>
2022-02-19 22:20:02 -0800

    Update 10:19

--- CougsHelpCougs/CougsHelpCougs/functions.cpp
+++ CougsHelpCougs/CougsHelpCougs/functions.cpp
@@ -19,3 +19,50 @@ void menu()
 {
 
 }
+
+User compare_users_hobby(User cougUsers[], int x)
+{
+	int n = 0;
+	int len = sizeof(cougUsers);
+	while(n < len -1)
+	{
+		
+        if (cougUsers[x].gethobby() == cougUsers[n].gethobby())
+		{
+			return cougUsers[n+1];
+		}
+	}
+		
+
+}
+
+User compare_users_academic_interest(User cougUsers[], int x)
+{
+	int n = 0;
+	int len = sizeof(cougUsers);
+	while(n < len -1)
+	{
+		
+        if (cougUsers[x].getacademic_interest() == cougUsers[n].getacademic_interest())
+		{
+			return cougUsers[n+1];
+		}
+	}
+		
+
+}
+
+User compare_users_recreation(User cougUsers[], int x)
+{
+	int n = 0;
+	int len = sizeof(cougUsers);
+	while(n < len -1)
+	{
+		
+        if (cougUsers[x].getrecreation() == cougUsers[n].getrecreation())
+		{
+			return cougUsers[n+1];
+		}
+	}
+		
+
